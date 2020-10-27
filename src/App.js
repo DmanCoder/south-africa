8c6d30130b55e8c623b01daeb1e7081001998d61
DmanCoder <ktdenis.1@live.com>
2020-10-27 12:11:41 +1000

    Basic body layout done

--- src/App.js
+++ src/App.js
@@ -23,54 +23,58 @@ const App = () => {
 
         <div className="info">
           <div className="twins">
-            <h3 className="info__title">
-              Africa is not a Destination. It's an Adventure set in Stunning
-              Scenery.
-            </h3>
-            <h5 className="info__sub-title">
-              Travel to Africa to experience the majesty of nature
-            </h5>
+            <div>
+              <h3 className="info__title">
+                Africa is not a Destination. It's an Adventure set in Stunning
+                Scenery.
+              </h3>
+              <h5 className="info__sub-title">
+                Travel to Africa to experience the majesty of nature
+              </h5>
 
-            <p className="info__text">
-              When it comes to incredible scenery, Africa is right up there with
-              the best. Boasting vast savannas, stunning beaches and dramatic
-              mountains, this continent has it all. Throw in epic canyons, sugar
-              plantations and extinct volcanoes, and you've got endless
-              opportunities to explore nature at its finest
-            </p>
-            <p className="info__text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis rem
-              quas soluta dicta excepturi tempora nulla doloribus nemo quo.
-            </p>
-          </div>
-          <div>
-            <AfricaMap />
-            <h4 className="info__quick-facts">
-              Africa, <br /> the quick facts
-            </h4>
+              <p className="info__text">
+                When it comes to incredible scenery, Africa is right up there
+                with the best. Boasting vast savannas, stunning beaches and
+                dramatic mountains, this continent has it all. Throw in epic
+                canyons, sugar plantations and extinct volcanoes, and you've got
+                endless opportunities to explore nature at its finest
+              </p>
+              <p className="info__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+                rem quas soluta dicta excepturi tempora nulla doloribus nemo
+                quo.
+              </p>
+            </div>
 
-            <p className="info__facts">
-              <span>Country Size</span>
-              <span>103,000 sq,km</span>
-            </p>
-            <p className="info__facts">
-              <span>Population (2015)</span>
-              <span>329.100</span>
-            </p>
-            <p className="info__facts">
-              <span>Capital</span>
-              <span>Reykjavik</span>
-            </p>
-            <p className="info__facts">
-              <span>Currency</span>
-              <span>Icelandic Krona (ISK)</span>
-            </p>
-            <p className="info__facts">
-              <span>GDP 32014 (MISK)</span>
-              <span>1.993.336</span>
-            </p>
+            <div>
+              <AfricaMap />
+              <h4 className="info__quick-facts">
+                Africa, <br /> the quick facts
+              </h4>
 
-            <button className="info__btn">More Facts On Africa</button>
+              <p className="info__facts">
+                <span>Country Size</span>
+                <span>103,000 sq,km</span>
+              </p>
+              <p className="info__facts">
+                <span>Population (2015)</span>
+                <span>329.100</span>
+              </p>
+              <p className="info__facts">
+                <span>Capital</span>
+                <span>Reykjavik</span>
+              </p>
+              <p className="info__facts">
+                <span>Currency</span>
+                <span>Icelandic Krona (ISK)</span>
+              </p>
+              <p className="info__facts">
+                <span>GDP 32014 (MISK)</span>
+                <span>1.993.336</span>
+              </p>
+
+              <button className="info__btn">More Facts On Africa</button>
+            </div>
           </div>
         </div>
 
@@ -147,7 +151,7 @@ const App = () => {
                 odit qui ipsam corporis aperiam tempore nemo doloribus ad!
               </p>
             </div>
-            <div className="red__img"></div>
+            <div className="rec__img2"></div>
           </div>
         </div>
       </div>
