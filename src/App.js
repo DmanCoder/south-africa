0a71b93222400fdf1e3c8446dbd782d2a8b028b8
DmanCoder <ktdenis.1@live.com>
2020-10-29 17:17:25 +1000

    Setup banner text out side

--- src/App.js
+++ src/App.js
@@ -10,91 +10,125 @@ import { ReactComponent as AfricaMap } from './assets/imgs/south-africa.svg';
 import './styles/main.scss';
 
 const App = () => {
-  const [isScrollDisabled, setIsScrollDisabled] = useState(false);
+  // Animating Flag - is out app animating
+  const [isAnimating, setIsAnimating] = useState(false);
 
-  const infoCT = useRef(null);
-  // const scrollToRef = (ref) => scroll.scrollTo(ref.current.offsetTop)
+  // Keep track of slide position
+  const [slidePosition, setSlidePosition] = useState(0);
 
-  // useEffect(() => {
-  //   let lastScrollTop = 0;
-  //   // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-  //   document.addEventListener(
-  //     'scroll',
-  //     () => {
-  //       // or window.addEventListener("scroll"....
-  //       const st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-  //       if (st > lastScrollTop) {
-  //         // downscroll code
+  // Height of the window
+  const pageHeight = window.innerHeight;
 
-  //         if (!isScrollDisabled) handleScrollMove(lastScrollTop);
-  //       } else {
-  //         // upscroll code
-  //         if (!isScrollDisabled) handleScrollMove(lastScrollTop);
-  //       }
-  //       lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-  //     },
-  //     false
-  //   );
-  // });
+  let aniSlideCT = useRef([]);
+  let slide1Banner = useRef(null);
+  let slide2Info1 = useRef(null);
+  let slide3Info2 = useRef(null);
+  let slide4Welcome = useRef(null);
+  let slide5Rec = useRef(null);
+
+  const slides = [
+    slide1Banner,
+    slide2Info1,
+    slide3Info2,
+    slide4Welcome,
+    slide5Rec,
+  ];
+
+  // let aniCurrentSlide = useRef()
 
   useEffect(() => {
-    console.log(window)
-    // gsap.to(window, {
-    //   duration: 1,
-    //   scrollTo: infoCT.current.offsetTop - 200,
-    // });
+    // mousewheel DOMMouseScroll
+    // document.addEventListener('mousewheel', handleOnMouseWheel);
 
-  }, []);
+    // Setup isScrolling variable
+    let isScrolling;
 
-  // const handleScrollMove = (position) => {
-  //   document.addEventListener('scroll', function () {
-  //     setIsScrollDisabled(true);
+    // Listen for scroll events
+    window.addEventListener(
+      // 'scroll',
+      'mousewheel',
+      (event) => {
+        // Clear our timeout throughout the scroll
+        window.clearTimeout(isScrolling);
+        // Set a timeout to run after scrolling ends
+        isScrolling = setTimeout(() => {
+          // Run the callback
+          console.log('Scrolling has stopped.', event);
+          handleOnMouseWheel(event);
+        }, 1000);
+      },
+      false
+    );
+  });
 
-  //     setTimeout(() => {
-  //       setIsScrollDisabled(false);
-  //     }, 2000);
-  //   });
-  // };
+  const handleOnMouseWheel = (event) => {
+    // Normalize event wheel delta
+    var delta = event.wheelDelta / 30 || -event.detail;
 
-  // useEffect(() => {
-  //   console.log(infoCT.current);
-  //   console.log(infoCT.current.offsetTop );
-  //   // Setup isScrolling variable
-  //   let isScrolling;
+    // If the user scrolled up, it goes to previous slide, otherwise - to next slide
+    if (delta < -1) {
+      // Go to next slide
+      handleGoToNextSlide();
+    } else if (delta > 1) {
+      // Go to previous slide
+      console.log('Go to previous slide');
+    }
+  };
 
-  //   // Listen for scroll events
-  //   window.addEventListener(
-  //     'scroll',
-  //     (event) => {
-  //       // Clear our timeout throughout the scroll
-  //       window.clearTimeout(isScrolling);
+  //  If there's a next slide, slide to it
+  const handleGoToNextSlide = () => {
+    const pos = slidePosition + 1;
+    if (slides[pos] === undefined) return;
 
-  //       // Set a timeout to run after scrolling ends
-  //       isScrolling = setTimeout(() => {
-  //         // Run the callback
-  //         console.log('Scrolling has stopped.');
-  //       }, 66);
-  //     },
-  //     false
-  //   );
-  // });
+    // Increment slide position by 1
+    // setSlidePosition(pos);
+    // handleGoToSlide(slides[pos]);
+  };
+
+  // Actual transition between slides
+  const handleGoToSlide = (slide) => {
+    // If the slides are not changing and there's such a slide
+    if (!isAnimating && slide.current) {
+      // Increment slide position by 1
+
+      console.log(slide, '============= ANIMATING');
+
+      // Setting animating flag to true
+      setIsAnimating(true);
+
+      gsap.to(aniSlideCT.current, {
+        duration: 1,
+        scrollTo: { y: slide.current.offsetTop - 200 },
+        onComplete: onSlideChangeEnd,
+        onCompleteScope: this,
+      });
+    }
+  };
+
+  /*
+   * Once the sliding is finished, we need to restore "isAnimating" flag.
+   * You can also do other things in this function, such as changing page title
+   */
+  const onSlideChangeEnd = () => setIsAnimating(false);
 
   return (
     <div>
       <Nav />
-
-      <div className="content">
-        <div className="banner">
+      <div className="banner-text">
+        <p className="banner-text__msg">Journey of the week</p>
+        <h1 className="banner-text__title">South Africa</h1>
+        <button className="banner-text__btn">
+          <span className="material-icons">add_circle</span>
+          <span>Read Article</span>
+        </button>
+      </div>
+      <div ref={aniSlideCT} className="slide-container">
+        <div ref={slide1Banner} className="banner">
           {/* ADD VIDEO PLAYER HERE */}
-          <p className="banner__msg">Journey of the week</p>
           <h1 className="banner__title">South Africa</h1>
-          <button className="banner__btn">
-            <span className="material-icons">add_circle</span>
-            <span>Read Article</span>
-          </button>
         </div>
 
-        <div ref={infoCT} className="info">
+        <div ref={slide2Info1} className="info">
           <div className="twins">
             <div>
               <h3 className="info__title">
@@ -108,7 +142,8 @@ const App = () => {
               <p className="info__text">
                 When it comes to incredible scenery, South Africa is right up
                 there with the best. Boasting vast savannas, stunning beaches
-                and dramatic mountains, this continent has it all. Quas soluta dicta excepturi.
+                and dramatic mountains, this continent has it all. Quas soluta
+                dicta excepturi.
               </p>
               <p className="info__text">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
@@ -149,7 +184,7 @@ const App = () => {
         </div>
 
         {/* RECOMMENDATION */}
-        <div className="rec">
+        <div ref={slide3Info2} className="rec">
           <div className="twins">
             <div className="rec__img"></div>
             <div className="rec__info">
@@ -177,7 +212,7 @@ const App = () => {
         </div>
 
         {/* WELCOME */}
-        <div className="welcome">
+        <div ref={slide4Welcome} className="welcome">
           <h3 className="welcome__title">
             Enjoy and Welcome to the World of South Africa!
           </h3>
@@ -191,7 +226,7 @@ const App = () => {
         </div>
 
         {/* RECOMMENDATION */}
-        <div className="rec">
+        <div ref={slide5Rec} className="rec">
           <div className="twins">
             <div className="rec__info">
               <h3 className="rec__title">Experience The Safari</h3>
